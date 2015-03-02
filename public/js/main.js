4b370255925483d4039850bb3c6e843ea45072b4
wrongkitchen <wkc.kenji@gmail.com>
2015-03-02 12:34:08 +0800

    test jquery animate in pi

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 var socket = io.connect('http://localhost:3000');
 	socket.on('news', function (data) {
-		console.log(data);
 		socket.emit('my other event', { my: 'data' });
 	});
+
+var startTheGame = function(){
+	$(".mainTitle").animate({ top:"-94px" });
+	$(".content").animate({ bottom:"-300px" });
+};
